7f4f2819d7b0225b9e5656b021e978f146ce6f8b
Daniel Garcia <dgarciaga@misena.edu.co>
2018-05-20 20:06:35 -0500

    Create controller show categories

---
+++ app/Http/Controllers/CategoriesApiController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\model\Category;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class CategoriesApiController extends Controller
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = User::find(1)->categories;
+        return response()->json($categories,200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $categories = User::find($id)->categories;
+        return response()->json($categories,200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/app/api/ArticleApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\app\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArticleApiController extends Controller
+{
+    //
+}
